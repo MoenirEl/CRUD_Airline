4790973e9133e7b41fd761bf26e11d17598ded64
MoenirEl <1186234@student.roc-nijmegen.nl>
2022-05-10 11:28:42 +0200

    login en nav

--- nav.php
+++ nav.php
@@ -5,9 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/nav.php">
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
-    
+    <nav>
+        <a href="index.php">home</a>
+        <a href="inloggen.php">login</a>
+        <a href="reserveren.php">reserveren</a>
+        <a href="about.php">about us</a>     
+    </nav>
+  
 </body>
 </html>
