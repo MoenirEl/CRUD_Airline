4e50cce411a4f81affe18ca5ad89dfab72d47494
Moenir <94520561+MoenirEl@users.noreply.github.com>
2022-06-24 13:44:14 +0200

    Moenir (#44)
    * login (#15)
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Roy (#11)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * Roy (#12)
    
    * login screen update en foto
    
    * achtergrond video toegevoegd
    
    * achtergrond video toegevoegd
    
    * Roy (#13)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * test (#14)
    
    * base
    
    * index
    
    * login en nav
    
    * Roy (#7)
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * Moenir (#5)
    
    * base
    
    * index
    
    * login en nav
    
    * delete main pages
    
    * Roy (#6)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Roy (#7)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Moenir (#8)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Moenir (#9)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * login foto
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>
    
    * index update
    
    * update login
    
    * index geupdated vid werkt en wat bugs gesquashed
    
    * niks
    
    * index update
    
    * fix
    
    * logo index update
    
    * contact
    
    * crud style
    
    * index en contact page klaar
    
    * play button
    
    * background change
    
    * ;)
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>

--- admin.php
+++ admin.php
@@ -2,67 +2,68 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>admin</title>
-  <link rel="stylesheet" href="css/admin.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link rel="stylesheet" href="css/admin.css">
 </head>
 
-<body class="sky" >
-    <a class="green" href="connectie/insert.php">insert  </a>
+<body class="sky">
+    <a class="green" href="connectie/insert.php">insert </a>
     <a class="blue" href="../CRUD_Space/index.php">home</a>
     <?php
-require_once "connectie/pdo.php";
+    require_once "connectie/pdo.php";
 
-$sql = "SELECT * FROM reizen";
-$stmt = $connect->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll();
-?>
-
-
-<div class="tablebox">
+    $sql = "SELECT * FROM reizen";
+    $stmt = $connect->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    ?>
 
 
-    <table>
-        <tr>
-            <th>planeetnaam </th>
-            <th>kosten </th>
-            <th>startdatum </th>
-            <th>einddatum </th>
-            <th>manierVanReizen </th>
-            <th>klasse </th>
-            <th>Personen </th>
-            <th>reis </th>
-            <th>beschrijving </th>
-        </tr>
-        
-        <?php foreach ($result as $re) { ?>
+    <div class="tablebox">
+
+
+        <table>
             <tr>
-                <td><?php echo $re["naam"]; ?></td>
-                <td><?php echo $re["kosten"]; ?></td>
-                <td><?php echo $re["startDatum"]; ?></td>
-                <td><?php echo $re["eindDatum"]; ?></td>
-                <td><?php echo $re["manierVanReizen"]; ?></td>
-                <td><?php echo $re["klasse"]; ?></td>
-                <td><?php echo $re["Personen"]; ?></td>
-                <td><?php echo $re["reis"]; ?></td>
-                <td><?php echo $re["beschrijving"]; ?></td>
-                <td>
-                    <a class="orange"href="connectie/edit.php?id=<?php echo $re["reisID"]; ?>">edit  </a>
-                    <a class="red" href="connectie/delete.php?id=<?php echo $re["reisID"]; ?>">delete  </a>
-                </td>
+                <th>planeetnaam </th>
+                <th>kosten </th>
+                <th>startdatum </th>
+                <th>einddatum </th>
+                <th>manierVanReizen </th>
+                <th>klasse </th>
+                <th>Personen </th>
+                <th>reis </th>
+                <th>beschrijving </th>
             </tr>
 
+            <?php foreach ($result as $re) { ?>
+                <tr>
+                    <td><?php echo $re["naam"]; ?></td>
+                    <td><?php echo $re["kosten"]; ?></td>
+                    <td><?php echo $re["startDatum"]; ?></td>
+                    <td><?php echo $re["eindDatum"]; ?></td>
+                    <td><?php echo $re["manierVanReizen"]; ?></td>
+                    <td><?php echo $re["klasse"]; ?></td>
+                    <td><?php echo $re["Personen"]; ?></td>
+                    <td><?php echo $re["reis"]; ?></td>
+                    <td><?php echo $re["beschrijving"]; ?></td>
+                    <td>
+                        <a class="orange" href="connectie/edit.php?id=<?php echo $re["reisID"]; ?>">edit </a>
+                        <a class="red" href="connectie/delete.php?id=<?php echo $re["reisID"]; ?>">delete </a>
+                    </td>
+                </tr>
 
-        <?php
-        }
-        ?>
-    </table>
-</div>
-  
+
+            <?php
+            }
+            ?>
+        </table>
+    </div>
+
 
 
 </body>
+
 </html>
--- nav.php
+++ nav.php
@@ -1,36 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital@1&family=Trispace&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/googlefonts.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/nav.css">
-  
-    
+
 </head>
+
 <body>
     <nav>
-        <img  id="logo" src="img/YR.png" alt="">
+        <img id="logo" src="img/YR.png" alt="">
         <a href="index.php">home</a>
-        <?php 
-            session_start();
-            if (isset($_SESSION['name'])) {
-                echo '<a href="admin.php">admin page</a>';
-            } else {
-                echo '<a href="inloggen.php">login</a>';
-            }
+        <?php
+        session_start();
+        if (isset($_SESSION['name'])) {
+            echo '<a href="admin.php">admin page</a>';
+        } else {
+            echo '<a href="inloggen.php">login</a>';
+        }
         ?>
         <a href="info.php">info</a>
-        <a href="about.php">about us</a> 
+        <a href="about.php">about us</a>
         <a href="contact.php">contact</a>
-        <?php 
-            if (isset($_SESSION['name'])) {
-                echo '<a href="connectie/sessionD.php">uitloggen</a>';
-            }
+        <?php
+        if (isset($_SESSION['name'])) {
+            echo '<a href="connectie/sessionD.php">uitloggen</a>';
+        }
         ?>
     </nav>
-  
+
 
 </body>
+
 </html>
