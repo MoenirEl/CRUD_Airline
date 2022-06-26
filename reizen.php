4a783f8e7b8801aa0b8c1e01d0edf47e4d388711
Moenir <94520561+MoenirEl@users.noreply.github.com>
2022-06-26 11:29:29 +0200

    Moenir (#50)
    * login (#15)
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Roy (#11)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * Roy (#12)
    
    * login screen update en foto
    
    * achtergrond video toegevoegd
    
    * achtergrond video toegevoegd
    
    * Roy (#13)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * test (#14)
    
    * base
    
    * index
    
    * login en nav
    
    * Roy (#7)
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * Moenir (#5)
    
    * base
    
    * index
    
    * login en nav
    
    * delete main pages
    
    * Roy (#6)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Roy (#7)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Moenir (#8)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Moenir (#9)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * login foto
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>
    
    * index update
    
    * update login
    
    * index geupdated vid werkt en wat bugs gesquashed
    
    * niks
    
    * index update
    
    * fix
    
    * logo index update
    
    * contact
    
    * crud style
    
    * index en contact page klaar
    
    * play button
    
    * background change
    
    * ;)
    
    * new page reizen
    
    * fix
    
    * index klaar contact page aangepast bugs gefixt en halvewege responsive klaar
    
    * responsive done
    
    * form bug fixt
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>

--- reizen.php
+++ reizen.php
@@ -8,7 +8,6 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/reizen.css">
     <link rel="stylesheet" href="css/footer.css">
-
 </head>
 
 <body>
@@ -52,7 +51,6 @@
 
         <div class="bottom_side">
             <div class="row_3 ruimteschip">
-
                 <h1 class="HR" >ruimteschip</h1>
                 <label class="labelR1" for=""> klasse 3</label>
                 <button class="ruimteB moveR">499 Euro</button>
@@ -66,13 +64,11 @@
             </div>
             <div class="row_3 wormhole">
                 <h1 class="HW">Wormgat</h1>
-
                 <button class="wormportB moveW">899 Euro</button>
                 <button class="afrekenen">afrekenen</button>
             </div>
             <div class="row_3 teleport">
                 <h1 class="HT">Teleportatie</h1>
-
                 <button class="wormportB moveT ">999 Euro</button>
 
 
