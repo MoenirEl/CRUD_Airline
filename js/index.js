2a9b305b903d9ca1c8559a1512cea2b26d40a37f
Void787 <1195841@student.roc-nijmegen.nl>
2022-06-26 11:30:58 +0200

    index.js verbertert

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-const button = document.querySelector('#indexformbutton');
+const form1 = document.querySelector('#van1');
+const form2 = document.querySelector('#van2');
+const form3 = document.querySelector('#van3');
+const formbutton1 = document.querySelector("#indexformbutton");
 
-button.addEventListener("click", (e) => {
-    if(username1.value == "" || password1.value == ""){
+formbutton1.addEventListener("click", (e) => {
+    if(form1.value == "" || form2.value == "" || form3.value == ""){
         e.preventDefault();
-        alert("both username or password cannot be emty");
+        alert("u moet in alle vakken iets selecteren");
+    } else{
+        e.preventDefault();
+        document.getElementById("form1").submit();
+        document.getElementById("form2").submit();
+        document.getElementById("form3").submit();
     }
 })
