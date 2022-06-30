f5b02bf9530b2a2c6bf53f3407ee875297970511
Moenir <94520561+MoenirEl@users.noreply.github.com>
2022-06-30 09:47:09 +0200

    Moenir (#59)
    * login (#15)
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Roy (#11)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * Roy (#12)
    
    * login screen update en foto
    
    * achtergrond video toegevoegd
    
    * achtergrond video toegevoegd
    
    * Roy (#13)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * index
    
    * poorten en schepen toegevoegd
    
    * login en nav
    
    * delete main pages
    
    * login foto
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    * test (#14)
    
    * base
    
    * index
    
    * login en nav
    
    * Roy (#7)
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * Moenir (#5)
    
    * base
    
    * index
    
    * login en nav
    
    * delete main pages
    
    * Roy (#6)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Roy (#7)
    
    * base
    
    * test Roy branch
    
    * test delete
    
    * CRUD in admin zetten
    
    * poorten en schepen toegevoegd
    
    * delete main pages
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    * Moenir (#8)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * Moenir (#9)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    * login foto
    
    * Moenir (#10)
    
    * base
    
    * index
    
    * login en nav
    
    * test
    
    * index
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    
    Co-authored-by: MoenirEl <1186234@student.roc-nijmegen.nl>
    Co-authored-by: Moenir <94520561+MoenirEl@users.noreply.github.com>
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>
    
    * index update
    
    * update login
    
    * index geupdated vid werkt en wat bugs gesquashed
    
    * niks
    
    * index update
    
    * fix
    
    * logo index update
    
    * contact
    
    * crud style
    
    * index en contact page klaar
    
    * play button
    
    * background change
    
    * ;)
    
    * new page reizen
    
    * fix
    
    * index klaar contact page aangepast bugs gefixt en halvewege responsive klaar
    
    * responsive done
    
    * form bug fixt
    
    * paar admin page toegevoegd
    
    * img
    
    * tekst update
    
    * tekst klaar
    
    * last min bugs
    
    * fix
    
    * index update
    
    Co-authored-by: Void787 <1195841@student.roc-nijmegen.nl>
    Co-authored-by: Void787 <94520895+Void787@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 
 <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital@1&family=Trispace&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="css/googlefonts.css">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital@1&family=Trispace&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/googlefonts.css">
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,7 +14,7 @@
 
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/footer.css">
-
+    <link rel="stylesheet" href="css/nav.css">
 
     <link rel="stylesheet" href="./dist/tavo-calendar.css" />
     <title>HoofdpaginaS</title>
@@ -31,62 +31,34 @@
     $stmt->execute();
     $result = $stmt->fetchAll();
     ?>
-      
+
 
     <div class="container">
+        <div class="up_space" ></div>
         <div class="textbox">
-            <h1  id="wittext" >heeft u ooit over nagedacht om naar de ruimte te gaan?</h1>
-        </div>
-            <form action="reizen.php" method="post" id="indexform">
-        <div class="uniblok">
-                   
-
-                <div class="routeblok">
-                    <div class="routeform" >
-                        <label for="Reis">reis</label>
-                        <select name="Reis" id="Reis">
-                            <option selected></option>
-                            <option value="retourreis">Retourreis</option>
-                            <option value="enkele reis">Enkele reis</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="reisvan">
-                    <div class="reisvanform" >
-                        <label for="van">Vertrek van</label>
-                        <select name="Van" id="Van">
-                            <option selected></option>
-                            <?php foreach ($result as $re) { ?>
-                                <option name="vertrek" ><?php echo $re["naam"]; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        
-                    </div>
-                    <div class="reisnaar">
-                        <div class="reisnaarform" >
-                            <label for="Naar">Aankomst op</label>
-                            <select name="Naar" id="Naar">
-                                <option selected></option>
-                                <?php foreach ($result as $re) { ?>
-                                    <option name="aankomst" ><?php echo $re["naam"]; ?></option>
-                                    <?php } ?>
-                                </select>
-                            </div>
-                            <?php if (isset($_SESSION['name'])) {
-                                    if ($_SESSION['authority'] == "admin" || $_SESSION['authority'] == "user" ) {
-                                        echo ' <button  type="submit"  id="indexformbutton" >verzend</button> ';
-                                    }
-                                    } else {
-                                        echo ("u moet eerst inloggen of registreren om een boeking te kunnen maken.");
-                                    }?>
-                        </form>
-            </div>
+            <h1 id="wittext">heeft u ooit over nagedacht om naar de ruimte te gaan?</h1>
         </div>
 
-        </div>
+            <div class="uniblok">
 
-        <div class="onderblok">
+
+                <?php if (isset($_SESSION['name'])) {
+                        if ($_SESSION['name'] == "admin" || $_SESSION['name'] == "user") {
+                            echo '<form action="reizen.php" method="post" id="indexform"> 
+                         <button  type="submit"  id="indexformbutton" >verzend</button>  </form>  ';
+                        }
+                    } else {
+                        echo '<form action="inloggen.php" method="post" id="indexform"> 
+                        <button type="submit" id="indexformbutton">inloggen</button>
+                        </form>';
+                    } ?> 
+                            
+    </div>
+    </div>
+
+    </div>
+
+    <div class="onderblok">
         <div class="col-2">
             <div id="login">
                 <h1 class="login_welkom">Log in of register </h1>
@@ -111,4 +83,5 @@
     <script type="text/javascript" src="js/particles.min.js"></script>
     <script type="text/javascript" src="js/custom.js"></script>
 </body>
+
 </html>
